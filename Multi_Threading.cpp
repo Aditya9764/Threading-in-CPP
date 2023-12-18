3eea3ade071455b50a7c5cfaf00d22d2f0b2778f
Aditya9764 <90107321+Aditya9764@users.noreply.github.com>
2023-12-18 14:35:46 +0530

    Create Multi_Threading.cpp

---
+++ Multi_Threading.cpp
@@ -0,0 +1,7 @@
+/* Multithreading is a specialized form of multitasking 
+multitasking is the feature that allows your computer to run two or more programs concurrently
+In general, there are two types of multitasking: process-based and thread-based.
+
+1)Process-based multitasking handles the concurrent execution of programs. Thread-based multitasking deals with the concurrent execution of pieces of the same program.
+2)Multithreaded program contains two or more parts that can run concurrently. Each part of such a program is called a thread, and each thread defines a separate path of execution.
+  */
